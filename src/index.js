58bc39a5eb48b08495e0baeaf2b53ebf8e9bf76a
liampc <kathliampcarlos@gmail.com>
2021-02-08 14:21:29 +0800

    move sass properties from app to index"

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import App from './components/App'
+import './scss/_Index.scss'
 
 
 ReactDOM.render(<App />, document.querySelector('#root'))
