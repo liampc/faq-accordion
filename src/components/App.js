54d9a9a1c529098c4513db7fc46e2b3eb7c05481
liampc <kathliampcarlos@gmail.com>
2021-02-08 14:57:20 +0800

    add card background

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import '../scss/_variables.scss'
 import '../scss/_App.scss'
 
 function App(){
     return (
        <main>
-
+           <div className="card">
+                <div className="card__hero"></div>
+                <div className="card__faq"></div>
+           </div>
        </main>
     )
 }
