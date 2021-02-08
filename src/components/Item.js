39a357d07b6181fa3e7a3f4e8649f27e9fcd8d51
liampc <kathliampcarlos@gmail.com>
2021-02-08 22:24:12 +0800

    edit Item component css

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,11 +3,15 @@ import React from 'react'
 import '../scss/_variables.scss'
 import '../scss/_Item.scss'
 
+import arrow from '../assets/images/icon-arrow-down.svg'
+
 function Item(){
     return (
-        <div>
-            <p>How many team members <span> / </span></p>
-            <p>No more than</p>
+        <div className="item">
+            <p className="item__question">How many team members 
+                <img src={arrow} alt="icon-arrow"/>
+            </p>
+            <p className="item__answer">No more than</p>
         </div>
     )
 }
