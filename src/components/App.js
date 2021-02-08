f808fbb6f1ca2276f08efdcb4e5bfb033a5e2c23
liampc <kathliampcarlos@gmail.com>
2021-02-08 21:33:34 +0800

    edit h1 and link Item component initial

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,17 @@ import React from 'react'
 import '../scss/_variables.scss'
 import '../scss/_App.scss'
 
+import Item from './Item'
+
 function App(){
     return (
        <main>
            <div className="card">
                 <div className="card__hero"></div>
-                <div className="card__faq"></div>
+                <div className="card__faq">
+                    <h1>FAQ</h1>
+                    <Item />
+                </div>
            </div>
        </main>
     )
---
+++ src/components/Item.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import '../scss/_variables.scss'
+import '../scss/_Item.scss'
+
+function Item(){
+    return (
+        <div>
+            <p>How many team members <span> / </span></p>
+            <p>No more than</p>
+        </div>
+    )
+}
+
+
+export default Item
