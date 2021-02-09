2494bdae384c46e1212ac516cba5adc69fffeb54
liampc <kathliampcarlos@gmail.com>
2021-02-09 14:20:41 +0800

    rewrite functional react to class based

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,37 @@
-import React from 'react'
+import React, {Component} from 'react'
 import '../scss/_variables.scss'
 import '../scss/_App.scss'
 
 import Item from './Item'
 import contentData from '../content'
 
-function App(){
+class App extends Component {
 
-    const content = contentData.map(data => {
-        return <Item list={data} key={data.question}/>
-    })
+    constructor(){
+        super()
+            this.state = {
+                contents: contentData
+            }
+    }
+    
+    render(){
 
-    return (
-       <main>
-           <div className="card">
-                <div className="card__hero"></div>
-                <div className="card__faq">
-                    <h1>FAQ</h1>
-                    {content}
+        const content = this.state.contents.map(data => {
+            return <Item list={data} key={data.question}/>
+        })
+
+        return (
+            <main>
+                <div className="card">
+                     <div className="card__hero"></div>
+                     <div className="card__faq">
+                         <h1>FAQ</h1>
+                         {content}
+                     </div>
                 </div>
-           </div>
-       </main>
-    )
+            </main>
+         )
+    }
 }
 
 
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,20 +1,26 @@
-import React from 'react'
+import React, {Component} from 'react'
 
 import '../scss/_variables.scss'
 import '../scss/_Item.scss'
 
 import arrow from '../assets/images/icon-arrow-down.svg'
 
-function Item(props){
-    return (
-        <div className="item">
-            <p className="item__question"> 
-                {props.list.question}
-                <img src={arrow} alt="icon-arrow"/>
-            </p>
-            <p className="item__answer">{props.list.answer}</p>
-        </div>
-    )
+
+
+class Item extends Component {
+
+    render (){
+        return (
+            <div className="item">
+                <p className="item__question"> 
+                    {this.props.list.question}
+                    <img src={arrow} alt="icon-arrow"/>
+                </p>
+                <p className="item__answer">{this.props.list.answer}</p>
+            </div>
+        )
+    }
+    
 }
 
 
